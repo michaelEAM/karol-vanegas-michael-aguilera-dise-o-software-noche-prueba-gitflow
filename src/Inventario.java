63caa3a87cabc037f97239a9d912795a747d61e6
karol <karolvanegas47@gmail.com>
2025-03-06 09:44:40 -0500

    metodoA

---
+++ src/Inventario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author karol
+ */
+public class Inventario {
+    
+     public void metodoA() { 
+        System.out.println("Ejecutando método A"); 
+    }
+    
+}
+s
